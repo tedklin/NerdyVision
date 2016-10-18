9e0b9b49761d0d845f2e8a7b47eae62e99cd8844
tedfoodlin <tedklin@gmail.com>
2016-10-17 17:55:11 -0700

    add user input for modes

--- NerdyVision.py
+++ NerdyVision.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import math
 import sys
 
-"""FRC Vision testing with OpenCV"""
+"""FRC Vision testing with OpenCV Python"""
 __author__ = "tedfoodlin"
 
 
@@ -13,16 +14,19 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 cap = cv2.VideoCapture(0)
 
 
-# ---------------- CONSTANTS ---------------- #
-# Modes (not recommended to turn on both modes)
-CAL_MODE_ON = True
-TRACK_MODE_ON = False
+# Set modes (if you don't want user input)
+CAL_MODE_ON = False
+TRACK_MODE_ON = True
 
-# HSV values
+# HSV range values for different colors
 LOWER_GREEN = np.array([30, 60, 60])
 UPPER_GREEN = np.array([70, 255, 255])
 LOWER_PINK = np.array([150, 60, 60])
 UPPER_PINK = np.array([170, 255, 255])
+
+# Set HSV range
+LOWER_LIM = LOWER_GREEN
+UPPER_LIM = UPPER_GREEN
 
 # Frame dimensions
 FRAME_Y = 716
@@ -30,17 +34,41 @@ FRAME_X = 1278
 FRAME_CENTER_Y = FRAME_Y / 2
 FRAME_CENTER_X = FRAME_X / 2
 
-# Calibration box
-CAL_UP = FRAME_CENTER_Y + 20
-CAL_LO = FRAME_CENTER_Y - 20
-CAL_R = FRAME_CENTER_X - 20
-CAL_L = FRAME_CENTER_X + 20
+# Calibration box dimensions
+CAL_AREA = 1600
+CAL_SIZE = int(math.sqrt(CAL_AREA))
+CAL_UP = FRAME_CENTER_Y + (CAL_SIZE / 2)
+CAL_LO = FRAME_CENTER_Y - (CAL_SIZE / 2)
+CAL_R = FRAME_CENTER_X - (CAL_SIZE / 2)
+CAL_L = FRAME_CENTER_X + (CAL_SIZE / 2)
 CAL_UL = (CAL_L, CAL_UP)
 CAL_LR = (CAL_R, CAL_LO)
-# ------------------------------------------- #
 
 
-# ---------------- FUNCTIONS ---------------- #
+def check_modes():
+    """Check which modes are on based on user input."""
+    if raw_input("Calibration mode on? (y/n)") == "y":
+        cal = True
+    else:
+        cal = False
+    if raw_input("Tracking mode on? (y/n)") == "y":
+        track = True
+    else:
+        track = False
+    return cal, track
+
+
+def calibration_box(img):
+    """Return HSV color in the calibration box."""
+    cv2.rectangle(img, CAL_UL, CAL_LR, (0, 255, 0), thickness=1)
+    roi = img[CAL_LO:CAL_UP, CAL_R:CAL_L]
+    average_color_per_row = np.average(roi, axis=0)
+    average_color = np.average(average_color_per_row, axis=0)
+    average_color = np.uint8([[average_color]])
+    hsv = cv2.cvtColor(average_color, cv2.COLOR_BGR2HSV)
+    return hsv
+
+
 def masking(lower, upper, frame):
     """Mask for specified color ranges."""
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -49,15 +77,14 @@ def masking(lower, upper, frame):
     return res, mask
 
 
-def calibration_box(img):
-    """Return HSV color in the calibration box"""
-    cv2.rectangle(img, CAL_UL, CAL_LR, (0, 255, 0), thickness=1)
-    roi = img[CAL_LO:CAL_UP, CAL_R:CAL_L]
-    average_color_per_row = np.average(roi, axis=0)
-    average_color = np.average(average_color_per_row, axis=0)
-    average_color = np.uint8([[average_color]])
-    hsv = cv2.cvtColor(average_color, cv2.COLOR_BGR2HSV)
-    return hsv
+def draw_static(img):
+    """Draw references on frame."""
+    # draw center of frame
+    cv2.circle(img, (FRAME_CENTER_X, FRAME_CENTER_Y), 5,
+               (0, 0, 255), -1)
+    # draw reference line for x position
+    cv2.line(img, (FRAME_CENTER_X, 0), (FRAME_CENTER_X, FRAME_Y),
+             (0, 0, 255), 2)
 
 
 def polygon(c):
@@ -97,34 +124,37 @@ def report_y(cy):
             print("Aim Lower")
         elif cy < FRAME_CENTER_Y - 10:
             print("Aim Higher")
-# ------------------------------------------- #
 
 
 def main():
+    # set modes (default without user input)
+    cal_mode_on = CAL_MODE_ON
+    track_mode_on = TRACK_MODE_ON
+
+    # turn on modes specified by user
+    # comment out next line if this feature is not desired
+    cal_mode_on, track_mode_on = check_modes()
+
     while True:
         ret, frame = cap.read()
 
         # calibration
-        if CAL_MODE_ON:
+        if cal_mode_on:
             print(np.array_str(calibration_box(frame)))
             cv2.imshow("NerdyCalibration", frame)
 
         # tracking
-        if TRACK_MODE_ON:
+        if track_mode_on:
             # init states (for x)
             turn_right = False
             turn_left = False
             ready = False
 
             # remove everything but specified color
-            res, mask = masking(LOWER_GREEN, UPPER_GREEN, frame)
+            res, mask = masking(LOWER_LIM, UPPER_LIM, frame)
 
-            # draw center of camera
-            cv2.circle(res, (FRAME_CENTER_X, FRAME_CENTER_Y), 5,
-                       (0, 0, 255), -1)
-            # draw line for x position (we have set shooting angles for y)
-            cv2.line(res, (FRAME_CENTER_X, 0), (FRAME_CENTER_X, FRAME_Y),
-                     (0, 0, 255), 2)
+            # draw references
+            draw_static(res)
 
             # find contour of goal
             cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
