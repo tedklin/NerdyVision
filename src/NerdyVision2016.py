d25f120b0ff6f577f9fe960e45a71a99c629bdd4
tedklin <tedklin@gmail.com>
2019-05-23 10:47:54 -0700

    gogogogogogog

--- src/NerdyVision2016.py
+++ src/NerdyVision2016.py
@@ -17,60 +17,26 @@ cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, NerdyConstants.FRAME_X)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, NerdyConstants.FRAME_Y)
 
+width_inches = 4
+fc_to_real_center_inches = 8
 
-# checks if the contour is tilted to the right
-def get_orientation(contour_corners): 
-    # contour_ab = contour_corners[0][1] - contour_corners[1][1]
-    # contour_ad = contour_corners[0][1] - contour_corners[3][1]
-    
-    # # 1 is oriented left, 2 is right, 3 is vertical
-    # if(contour_ab < contour_ad):
-    #     return 1
-    # elif(contour_ab > contour_ad):
-    #     return 2
-    # elif(contour_corners[2][0] == contour_corners[1][0] or contour_corners[2][1] == contour_corners[2][1]):
-    #     return 3
-
-    dy_1 = contour_corners[0][1] - contour_corners[1][1]
-    dx_1 = contour_corners[0][0] - contour_corners[1][0]
-    hypo_1 = math.sqrt(math.pow(dy_1, 2) + math.pow(dx_1, 2))
-
-    dy_2 = contour_corners[1][1] - contour_corners[2][1]
-    dx_2 = contour_corners[1][0] - contour_corners[2][0]
-    hypo_2 = math.sqrt(math.pow(dy_2, 2) + math.pow(dx_2, 2))
-
-    if hypo_2 > hypo_1:
-        return 1
-    elif hypo_1 > hypo_2:
-        return 2
+def pixels_to_inches(pixels, width_pixels):
+    if width_pixels == 0:
+        return 0
     else:
-        return 3
+        return pixels * width_inches / width_pixels
+
+
+def inches_to_pixels(inches, width_pixels):
+    if width_pixels == 0:
+        return 0
+    else: 
+        return inches * width_pixels / width_inches
 
 
 def main():
 
-    # brightness adjusted, used to be 30, now is 70
-    # os.system("v4l2-ctl -d /dev/video0 "
-    #           "-c brightness=70 "
-    #           "-c contrast=10 "
-    #           "-c saturation=100 "
-    #           "-c white_balance_temperature_auto=0 "
-    #           "-c power_line_frequency=2 "
-    #           "-c white_balance_temperature=4500 "
-    #           "-c sharpness=25 "
-    #           "-c backlight_compensation=0 "
-    #           "-c exposure_auto=1 "
-    #           "-c exposure_absolute=5 "
-    #           "-c pan_absolute=0 "
-    #           "-c tilt_absolute=0 "
-    #           "-c zoom_absolute=0")
-
-    angle_to_turn = 0
-
     while 687:
-
-        aligned = False
-        previous_angle_to_turn = angle_to_turn
 
         ret, frame = cap.read()
         capture_time = time.time()
@@ -87,66 +53,95 @@ def main():
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
             area = cv2.contourArea(c)
+            goal = NerdyFunctions.polygon(c, 0.025)
+            M = cv2.moments(goal)
 
-            if area > NerdyConstants.MIN_GOAL_AREA:
-                goal = NerdyFunctions.polygon(c, 0.025)
+            if area > NerdyConstants.MIN_GOAL_AREA and len(goal) == 4 and M['m00'] > 0:
+                cv_cx, cv_cy = NerdyFunctions.calc_center(M)
+                center = (cv_cx, cv_cy)
 
-                if len(goal) == 4:
-                    M = cv2.moments(goal)
+                # DISPLAY BLOCK REMEMBER TO COMMENT OUT
+                cv2.circle(res, center, 5, (255, 0, 0), -1) # remember to comment out
+                
+                real_cx = cv_cx - (NerdyConstants.FRAME_CX)
+                real_cy = (NerdyConstants.FRAME_CY) - cv_cy
+                center = (real_cx, real_cy)
+                # print(center)
 
-                    if M['m00'] > 0:
-                        cv_cx, cv_cy = NerdyFunctions.calc_center(M)
-                        center = (cv_cx, cv_cy)
-                        # DISPLAY BLOCK REMEMBER TO COMMENT OUT
-                        cv2.circle(res, center, 5, (255, 0, 0), -1) # remember to comment out
-                        
-                        real_cx = cv_cx - (NerdyConstants.FRAME_CX)
-                        real_cy = (NerdyConstants.FRAME_CY) - cv_cy
-                        center = (real_cx, real_cy)
-                        print(center)
+                rect = cv2.minAreaRect(goal)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
 
-                        rect = cv2.minAreaRect(goal)
-                        box = cv2.boxPoints(rect)
-                        box = np.int0(box)
+                # orientation = get_orientation(box)
+                # print(orientation)
+                dy_1 = box[0][1] - box[1][1]
+                dx_1 = box[0][0] - box[1][0]
+                hypo_1 = math.sqrt(math.pow(dy_1, 2) + math.pow(dx_1, 2))
 
-                        orientation = get_orientation(box)
-                        print(orientation)
-                        if (orientation == 1):
-                            dy = abs(box[2][1] - box[1][1])
-                            dx = box[2][0] - box[1][0]
-                            angle = 90 - math.degrees(math.atan2(dy, dx))
-                            slope = dy / dx * 1.0
-                            y_int = slope * -real_cx + real_cy
-                        elif (orientation == 2):
-                            dy = abs(box[1][1] - box[0][1])
-                            dx = box[1][0] - box[0][0]
-                            angle = 90 - math.degrees(math.atan2(dy, dx))
-                            slope = dy / dx * 1.0
-                            y_int = slope * -real_cx + real_cy
-                        else:
-                            dy = 0
-                            dx = 1
-                            angle = 0
-                            slope = 0
-                            y_int = 99999
-                        if math.isinf(y_int):
-                            y_int = 99999
-                        else:
-                            y_int = int(y_int)
+                dy_2 = box[1][1] - box[2][1]
+                dx_2 = box[1][0] - box[2][0]
+                hypo_2 = math.sqrt(math.pow(dy_2, 2) + math.pow(dx_2, 2))
 
-                        print(dy)
-                        print(dx)
-                        print(angle)
-                        print(slope)
-                        print(y_int)
-                        print()
+                if (hypo_2 > hypo_1):
+                    dy = abs(box[2][1] - box[1][1])
+                    dx = box[2][0] - box[1][0]
+                    angle = 90 - math.degrees(math.atan2(dy, dx))
+                    slope = dy / dx * 1.0
+                    y_int = slope * -real_cx + real_cy
+                    width_pixels = hypo_1
+                elif (hypo_1 > hypo_2):
+                    dy = abs(box[1][1] - box[0][1])
+                    dx = box[1][0] - box[0][0]
+                    angle = 90 - math.degrees(math.atan2(dy, dx))
+                    slope = dy / dx * 1.0
+                    y_int = slope * -real_cx + real_cy
+                    width_pixels = hypo_2
+                else:
+                    dy = 0
+                    dx = 1
+                    angle = 0
+                    slope = 0
+                    y_int = 99999
+                    width_pixels = 0
 
-                        cv_y_int = (NerdyConstants.FRAME_CX, (NerdyConstants.FRAME_CY - y_int))
-                        cv2.circle(res, cv_y_int, 5, (0, 255, 0), -1) # remember to comment out
+                if math.isinf(y_int):
+                    y_int = 99999
+                else:
+                    y_int = int(y_int)
 
-                        # DISPLAY BLOCK REMEMBER TO COMMENT OUT
-                        # cv2.drawContours(res, [goal], 0, (255, 0, 0), 5)
-                        cv2.drawContours(res,[box],0,(255,0,0),2)
+                # print(dy)
+                # print(dx)
+                # print(angle)
+                # print(slope)
+                # print(y_int)
+                print()
+
+                cv_y_int = (NerdyConstants.FRAME_CX, (NerdyConstants.FRAME_CY - y_int))
+                cv2.circle(res, cv_y_int, 5, (0, 255, 0), -1) # remember to comment out
+
+                # DISPLAY BLOCK REMEMBER TO COMMENT OUT
+                # cv2.drawContours(res, [goal], 0, (255, 0, 0), 5)
+                cv2.drawContours(res,[box],0,(255,0,0),2)
+
+                # we need to send real_cx, real_cy, angle, y_int
+                real_cx_inches = pixels_to_inches(real_cx, width_pixels)
+                real_cy_inches = pixels_to_inches(real_cx, width_pixels)
+                y_int_inches = pixels_to_inches(y_int, width_pixels)
+
+                print(real_cx_inches)
+                print(real_cy_inches)
+                print(y_int_inches)
+
+                offset_inches = inches_to_pixels(fc_to_real_center_inches, width_pixels)
+                real_cy_inches = real_cy_inches + offset_inches
+                y_int_inches = y_int_inches + offset_inches
+
+                # stuff to send
+                # print(offset_inches)
+                # print(real_cx_inches)
+                # print(real_cy_inches)
+                # print(y_int_inches)
+                # print(angle)
 
 
         # DISPLAY BLOCK REMEMBER TO COMMENT OUT
